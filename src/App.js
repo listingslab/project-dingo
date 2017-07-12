330c4e900c050af386b9c111a8df8fa0bfe0c8c0
Chris Dorward <listingslab@gmail.com>
2017-07-12 21:28:24 +0930

    "version": "0.1.2",

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React App with PWA support</h2>
+          <h2>Project Dingo</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          version 0.1.2
         </p>
       </div>
     );
